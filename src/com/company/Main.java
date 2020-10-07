6c28708928b5c0b3203655a29a82fe05e90ede76
IonTank5 <61195075+IonTank5@users.noreply.github.com>
2020-10-07 16:35:07 -0400

    I probably made a change idek

---
+++ src/com/company/Main.java
@@ -0,0 +1,52 @@
+package com.company;
+import java.util.Scanner;
+import java.io.File;
+public class Main {
+
+    public static void main(String[] args){
+        try {
+            Scanner in = new Scanner(System.in); // Getting user input later on
+            System.out.println("Enter filepath for your file. IF file is invalid, the bee movie will be used");
+            String input = in.nextLine(); // Getting file path
+            File script;
+            boolean check = false;
+            try{
+                script = new File(input);
+                Scanner read = new Scanner(script); // Checks to see if it can create a scanner for the file, it seemed to be the best way to test it
+                System.out.println("File check successful!");
+                check = true;
+            }
+            catch (Exception f){
+                System.out.println("Invalid file. Bee movie script will be used");
+                script = new File("C:\\Users\\jaked\\IdeaProjects\\wordifier\\BeeMovieScript.txt"); //This is just where I am keeping it, it will be wherever you installed this github repository
+            }
+            Scanner read = new Scanner(script); // Resetting the scanner
+            String word1, word2; // word1 is the word being replaced, word2 is the word replacing it
+            System.out.println("Enter word to replace: ");
+            word1 = in.next();
+            System.out.println("Enter word to replace " + word1 + " with: ");
+            word2 = in.next();
+            while(read.hasNext()){ //Checks if there is another word in the file
+                String temp = "";
+                String wordChecker = "";
+                boolean checker = false;
+                temp = read.next();
+                for (int i = 0; i < temp.length()-(word1.length()-1); i++) {
+                    wordChecker = "";
+                    for (int j = 0; j < word1.length(); j++) {
+                        wordChecker+=(temp.substring(i + j, i + j + 1));
+                    }
+                    if (wordChecker.toString().equalsIgnoreCase(word1)){
+                        System.out.print(temp.substring(0, i) + word2+ temp.substring(i + word1.length()) + " ");
+                        checker = true;
+                        break;
+                    }
+                }
+                if(checker == false)
+                    System.out.print(temp + " ");
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+}
